6c81f46429c0ffb8483ecfe459598912a2fbf684
Martynas <nbtsst@gmail.com>
2021-05-10 23:41:37 +0300

    Initial project setup

---
+++ src/main/java/com/martynasvq/webchat/WebchatApplication.java
@@ -0,0 +1,14 @@
+package com.martynasvq.webchat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martynasvq/webchat/WebchatApplicationTests.java
@@ -0,0 +1,15 @@
+package com.martynasvq.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
